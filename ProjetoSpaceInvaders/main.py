21106696be9da7c7867fee8f3d9f65920849de7c
Guilherme Vianna de Moura <88854722+GuilhermeViannaMoura@users.noreply.github.com>
2021-08-15 00:36:13 -0300

    Add files via upload

--- ProjetoSpaceInvaders/main.py
+++ ProjetoSpaceInvaders/main.py
@@ -21,6 +21,10 @@ game_background = GameImage("LabJogos/ProjetoSpaceInvaders/imagens/starBackgroun
 tiros = []
 inimigos = cria_inimigos()
 vel_inimigo = 100
+relogio = 0
+nFrames = 0
+fps = 0
+pontos = 0
 
 #GAME LOOP
 while True:
@@ -33,8 +37,16 @@ while True:
         vel_inimigo = colisao_inimigo_parede(inimigos,janela,vel_inimigo)
         desenha_inimigos(inimigos,vel_inimigo,janela)
         pMove(nave,teclado,janela)
-        atira(teclado,tiros,nave,janela)
-
+        projeteis = atira(teclado,tiros,nave,janela)
+        colisao_tiro_inimigo(projeteis,inimigos,janela)            
+        #FPS
+        relogio += janela.delta_time()
+        nFrames += 1
+        if relogio>=1:
+            fps = nFrames
+            nFrames = 0
+            relogio = 0
+        janela.draw_text("FPS: %d" %fps,10,10,size=20,color=(0,0,0),font_name="Arial",bold=True)
         #keyboard interactions
         if teclado.key_pressed("ESC"): #volta pro menu
             controle = "menu"
